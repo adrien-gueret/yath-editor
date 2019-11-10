0420f9f81331f6edbf140f1f8afd6d9481338ce4
Adrien Guéret <adrien.grt@gmail.com>
2019-11-10 23:40:07 +0100

    refactor(Board): use hooks instead of connect

--- src/modules/app/components/Board/Board.js
+++ src/modules/app/components/Board/Board.js
@@ -1,8 +1,7 @@
 import './board.less';
 
 import React from 'react';
-import PropTypes from 'proptypes';
-import { connect } from 'react-redux'
+import { shallowEqual, useSelector } from 'react-redux'
 
 import screensSelectors from 'Modules/screens/selectors';
 import appSelectors from 'Modules/app/selectors';
@@ -10,37 +9,21 @@ import ScreenEdit from 'Modules/screens/components/ScreenEdit';
 import Screen from 'Modules/screens/components/Screen';
 import ArrowsBoard from '../ArrowsBoard';
 
-class Board extends React.Component {
-    static propTypes = {
-        moveScreen: PropTypes.func,
-        screens: PropTypes.array,
-        editScreenId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    };
+function Board() {
+    const editScreenId = useSelector(appSelectors.getEditScreenId);
+    const screens = useSelector(screensSelectors.getAsArray, shallowEqual);
 
-    static defaultProps = {
-        moveScreen() {},
-        screens: [],
-        editScreenId: null,
-    };
-
-    render() {
-        return (
-            <section className="yathBoard">
-                {
-                    this.props.screens.map(screen =>  (
-                        <Screen key={ screen.id } screenId={screen.id} />
-                    ))
-                }
-                { this.props.editScreenId && <ScreenEdit /> }
-                <ArrowsBoard />
-            </section>
-        );
-    }
+    return (
+        <section className="yathBoard">
+            {
+                screens.map(screen =>  (
+                    <Screen key={ screen.id } screenId={screen.id} />
+                ))
+            }
+            { editScreenId && <ScreenEdit /> }
+            <ArrowsBoard />
+        </section>
+    );
 }
 
-const mapStateToProps = (state) => ({
-    editScreenId: appSelectors.getEditScreenId(state),
-    screens: screensSelectors.getAsArray(state),
-});
-
-export default connect(mapStateToProps)(Board);
+export default Board;
