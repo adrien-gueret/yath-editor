18dbe5678dd3f05e7b6d44f81911f22a466e826b
Adrien Guéret <adrien.grt@gmail.com>
2020-02-25 23:36:27 +0100

    refactor(board,arrows): remove less to use jss

--- src/modules/board/components/ArrowsBoard/ArrowsBoard.js
+++ src/modules/board/components/ArrowsBoard/ArrowsBoard.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import { shallowEqual, useSelector } from 'react-redux';
 
+import { makeStyles } from '@material-ui/core';
+
 import { selectors as screenSelectors } from 'Modules/screens';
-import './arrowsBoard.less';
+
+const useStyles = makeStyles(({ palette }) => ({
+    root: {
+        pointerEvents: 'none',
+        zIndex: 0,
+        position: 'relative',
+        width: '100%',
+        height: '100%',
+    },
+    marker: {
+        fill: palette.common.black,
+        stroke: palette.common.white,
+    },
+    arrow: {
+        stroke: 'rgba(0, 0, 0, .4)',
+        fill: 'blue',
+        strokeWidth: '2px',
+        markerEnd: 'url(#arrow)',
+    },
+}), { classNamePrefix: 'ArrowsBoard' });
 
 function ArrowsBoard () {
     const arrows = useSelector(screenSelectors.list.getArrows, shallowEqual);
+    const classes = useStyles();
 
     return (
-        <svg width="100%" height="100%" className="arrowsBoard">
+        <svg width="100%" height="100%" className={classes.root}>
             <defs>
                 <marker id="arrow" markerWidth="13" markerHeight="13" refX="10" refY="6" orient="auto">
-                    <path d="M2,1 L2,10 L10,6 L2,2" className="arrowsBoard__arrowMarker" />
+                    <path d="M2,1 L2,10 L10,6 L2,2" className={classes.marker} />
                 </marker>
             </defs>
             {
@@ -19,7 +41,7 @@ function ArrowsBoard () {
                     <path
                         key={index}
                         d={`M${arrow.start.x},${arrow.start.y} L${arrow.end.x},${arrow.end.y}`}
-                        className="arrowsBoard__arrow"
+                        className={classes.arrow}
                      />
                 ))
             }
