92cc8a4c31c48f8e2f2f3307682475694b85c43c
Adrien Guéret <adrien.grt@gmail.com>
2020-04-05 13:52:01 +0200

    refactor(app): remove .less file

--- src/modules/app/components/App/App.js
+++ src/modules/app/components/App/App.js
@@ -1,13 +1,24 @@
-import './app.less';
-
 import React from 'react';
 import { useSelector } from 'react-redux';
+
+import { makeStyles } from '@material-ui/core';
 
 import AppHeader from '../AppHeader';
 
 import { Board } from 'Modules/Board';
 import { GameTest, GameConfiguration, selectors as gameSelectors } from 'Modules/game';
 import { selectors as screenSelectors, ScreenEdit } from 'Modules/screens';
+
+const useStyles = makeStyles(() => ({
+    '@global': {
+        body: {
+            margin: 0,
+        },
+    },
+    root: {
+        paddingTop: 50,
+    }
+}), { classNamePrefix: 'App' });
 
 export function App() {
     const editedScreenId = useSelector(screenSelectors.editedScreenId.get);
@@ -16,10 +27,12 @@ export function App() {
 
     const startScreenId = typeof isTesting === 'boolean' ? null : isTesting;
 
-   const isDialogOpen = Boolean(editedScreenId) || isTesting || isConfiguring;
+    const isDialogOpen = Boolean(editedScreenId) || isTesting || isConfiguring;
+
+    const classes = useStyles();
 
     return (
-        <div className="yathApp">
+        <div className={classes.root}>
             <AppHeader />
             <Board isDialogOpen={isDialogOpen} />
             { isTesting && <GameTest startScreenId={startScreenId} /> }
