8ba240ecc2bb50d8992462ea78f9c8095ef2fc4f
Adrien Guéret <adrien.grt@gmail.com>
2022-03-13 11:19:00 +0100

    fix(logiv,results): check correctly RESET_HISTORY result

--- src/modules/logic/constants/resultToValueType.js
+++ src/modules/logic/constants/resultToValueType.js
@@ -1,9 +1,10 @@
 import {
-    REDIRECT, ADD_ITEM, REMOVE_ITEM
+    REDIRECT, ADD_ITEM, REMOVE_ITEM, SWITCH_SCREEN_CONTENT,
 } from './results';
 
 export default {
     [REDIRECT]: 'screen',
     [ADD_ITEM]: 'item',
     [REMOVE_ITEM]: 'item',
+    [SWITCH_SCREEN_CONTENT]: 'screen-content',
 };
