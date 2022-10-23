0ed1d73ba4bac9076acaf435fac699b306f307a5
Adrien Guéret <adrien.grt@gmail.com>
2022-10-24 01:27:09 +0200

    fix(screens): avoid crash when trying to manipulate a deleted screen

--- src/modules/screens/reducers/list.js
+++ src/modules/screens/reducers/list.js
@@ -4,6 +4,7 @@ import ScreenModel from '../models/Screen';
 
 import actionTypes from '../actions/types';
 
+
 import { actionTypes as linkActionTypes } from 'Modules/links';
 import { actionTypes as logicActionTypes } from 'Modules/logic';
 
@@ -18,250 +19,268 @@ const INITIAL_STATE = {
 };
 
 export default function list(state = INITIAL_STATE, action) {
-    switch (action.type) {
-        case actionTypes.ADD_SCREEN:
-            return {
-                ...state,
-                [action.payload.screen.id]: action.payload.screen,
-            };
+    const getScreenCloneByPayloadScreenId = (screenToCloneId = action.payload.screenId) => {
+        const correspondingScreen = state[screenToCloneId];
 
-        case actionTypes.DELETE_SCREEN: {
-            const screenToDelete = state[action.payload.screenId];
-
-            if (!screenToDelete) {
-                return state;
-            }
-
-            const newScreens = { ...state };
-            delete newScreens[screenToDelete.id];
-
-            return newScreens;
+        if (!correspondingScreen) {
+            throw new ReferenceError();
         }
 
-        case actionTypes.DELETE_ALL_SCREENS: {
-            return {};
-        }
+        return correspondingScreen.clone();
+    };
 
-        case actionTypes.EDIT_SCREEN_NAME: {
-            let newName = action.payload.newName;
-            let alreadyExisted = false;
-
-            do {
-                const newSlug = new ScreenModel({ name: newName, id: 1 }).getSlug();
-                alreadyExisted = Object.keys(state).some(screenId => state[screenId].getSlug() === newSlug);
-
-                if (alreadyExisted) {
-                    newName += '_2';
+    try {
+        switch (action.type) {
+            case actionTypes.ADD_SCREEN:
+                return {
+                    ...state,
+                    [action.payload.screen.id]: action.payload.screen,
+                };
+    
+            case actionTypes.DELETE_SCREEN: {
+                const screenToDelete = state[action.payload.screenId];
+    
+                if (!screenToDelete) {
+                    return state;
                 }
-            } while(alreadyExisted);
-
-            const newScreen = state[action.payload.screenId].clone();
-            newScreen.name = newName;
-
-            return {
-                ...state,
-                [action.payload.screenId]: newScreen,
-            };
-        }
-
-        case actionTypes.EDIT_SCREEN_CONTENT: {
-            const newScreen = state[action.payload.screenId].clone();
-            newScreen.content = action.payload.newContent === '<p></p>' ? '' : action.payload.newContent;
-
-            return {
-                ...state,
-                [action.payload.screenId]: newScreen,
-            };
-        }
-
-        case actionTypes.ADD_ALTERNATIVE_SCREEN_CONTENT: {
-            const newScreen = state[action.payload.screenId].clone();
-            newScreen.alternativeContents = [...newScreen.alternativeContents, { id: shortid.generate(), value: '' }];
-
-            return {
-                ...state,
-                [action.payload.screenId]: newScreen,
-            };
-        }
-
-        case actionTypes.EDIT_ALTERNATIVE_SCREEN_CONTENT: {
-            const newScreen = state[action.payload.screenId].clone();
-
-            const alternativeContent = newScreen.alternativeContents.find(({ id }) => id === action.payload.contentId);
-
-            if (!alternativeContent) {
-                return state;
+    
+                const newScreens = { ...state };
+                delete newScreens[screenToDelete.id];
+    
+                return newScreens;
             }
-
-            const newValue = action.payload.newContent === '<p></p>' ? '' : action.payload.newContent;
-
-            newScreen.alternativeContents = newScreen.alternativeContents.map(({ id, value }) => (
-                id !== action.payload.contentId ? { id, value } : { id, value: newValue }
-            ));
-
-            return {
-                ...state,
-                [action.payload.screenId]: newScreen,
-            };
-        }
-
-        case actionTypes.DELETE_ALTERNATIVE_SCREEN_CONTENT: {
-            const newScreen = state[action.payload.screenId].clone();
-            newScreen.alternativeContents = newScreen.alternativeContents.filter(({ id }) => id !== action.payload.contentId);
-
-            return {
-                ...state,
-                [action.payload.screenId]: newScreen,
-            };
-        }
-
-        case actionTypes.EDIT_SCREEN_IMAGE: {
-            const newScreen = state[action.payload.screenId].clone();
-            newScreen.image = action.payload.newImage;
-
-            return {
-                ...state,
-                [action.payload.screenId]: newScreen,
-            };
-        }
-
-        case actionTypes.EDIT_SCREEN_IMAGE_ORDER: {
-            const newScreen = state[action.payload.screenId].clone();
-            newScreen.mustRenderImageAfterContent = action.payload.mustRenderAfterContent;
-
-            return {
-                ...state,
-                [action.payload.screenId]: newScreen,
-            };
-        }
-
-        case actionTypes.SET_START_SCREEN: {
-            return Object.keys(state)
-                .reduce((newState, screenId) => {
-                    const clonedScreen = state[screenId].clone();
-                    clonedScreen.isStart = clonedScreen.id === action.payload.screenId;
-
+    
+            case actionTypes.DELETE_ALL_SCREENS: {
+                return {};
+            }
+    
+            case actionTypes.EDIT_SCREEN_NAME: {
+                let newName = action.payload.newName;
+                let alreadyExisted = false;
+    
+                do {
+                    const newSlug = new ScreenModel({ name: newName, id: 1 }).getSlug();
+                    alreadyExisted = Object.keys(state).some(screenId => state[screenId].getSlug() === newSlug);
+    
+                    if (alreadyExisted) {
+                        newName += '_2';
+                    }
+                } while(alreadyExisted);
+    
+                const newScreen = getScreenCloneByPayloadScreenId();
+                newScreen.name = newName;
+    
+                return {
+                    ...state,
+                    [action.payload.screenId]: newScreen,
+                };
+            }
+    
+            case actionTypes.EDIT_SCREEN_CONTENT: {
+                const newScreen = getScreenCloneByPayloadScreenId();
+                newScreen.content = action.payload.newContent === '<p></p>' ? '' : action.payload.newContent;
+    
+                return {
+                    ...state,
+                    [action.payload.screenId]: newScreen,
+                };
+            }
+    
+            case actionTypes.ADD_ALTERNATIVE_SCREEN_CONTENT: {
+                const newScreen = getScreenCloneByPayloadScreenId();
+                newScreen.alternativeContents = [...newScreen.alternativeContents, { id: shortid.generate(), value: '' }];
+    
+                return {
+                    ...state,
+                    [action.payload.screenId]: newScreen,
+                };
+            }
+    
+            case actionTypes.EDIT_ALTERNATIVE_SCREEN_CONTENT: {
+                const newScreen = getScreenCloneByPayloadScreenId();
+    
+                const alternativeContent = newScreen.alternativeContents.find(({ id }) => id === action.payload.contentId);
+    
+                if (!alternativeContent) {
+                    return state;
+                }
+    
+                const newValue = action.payload.newContent === '<p></p>' ? '' : action.payload.newContent;
+    
+                newScreen.alternativeContents = newScreen.alternativeContents.map(({ id, value }) => (
+                    id !== action.payload.contentId ? { id, value } : { id, value: newValue }
+                ));
+    
+                return {
+                    ...state,
+                    [action.payload.screenId]: newScreen,
+                };
+            }
+    
+            case actionTypes.DELETE_ALTERNATIVE_SCREEN_CONTENT: {
+                const newScreen = getScreenCloneByPayloadScreenId();
+                newScreen.alternativeContents = newScreen.alternativeContents.filter(({ id }) => id !== action.payload.contentId);
+    
+                return {
+                    ...state,
+                    [action.payload.screenId]: newScreen,
+                };
+            }
+    
+            case actionTypes.EDIT_SCREEN_IMAGE: {
+                const newScreen = getScreenCloneByPayloadScreenId();
+                newScreen.image = action.payload.newImage;
+    
+                return {
+                    ...state,
+                    [action.payload.screenId]: newScreen,
+                };
+            }
+    
+            case actionTypes.EDIT_SCREEN_IMAGE_ORDER: {
+                const newScreen = getScreenCloneByPayloadScreenId();
+                newScreen.mustRenderImageAfterContent = action.payload.mustRenderAfterContent;
+    
+                return {
+                    ...state,
+                    [action.payload.screenId]: newScreen,
+                };
+            }
+    
+            case actionTypes.SET_START_SCREEN: {
+                return Object.keys(state)
+                    .reduce((newState, screenId) => {
+                        const clonedScreen = getScreenCloneByPayloadScreenId(screenId);
+                        clonedScreen.isStart = clonedScreen.id === action.payload.screenId;
+    
+                        return {
+                            ...newState,
+                            [clonedScreen.id]: clonedScreen,
+                        };
+                    }, {});
+            }
+    
+            case actionTypes.MOVE_SCREEN: {
+                const newScreen = getScreenCloneByPayloadScreenId();
+    
+                newScreen.x = Math.max(0, action.payload.newX);
+                newScreen.y = Math.max(0, action.payload.newY);
+    
+                return {
+                    ...state,
+                    [action.payload.screenId]: newScreen,
+                };
+            }
+    
+            case actionTypes.SELECT_SCREEN: {
+                const newScreen = getScreenCloneByPayloadScreenId();
+    
+                newScreen.isSelected = true;
+    
+                return {
+                    ...state,
+                    [action.payload.screenId]: newScreen,
+                };
+            }
+    
+            case actionTypes.UNSELECT_SCREEN: {
+                const newScreen = getScreenCloneByPayloadScreenId();
+    
+                newScreen.isSelected = false;
+    
+                return {
+                    ...state,
+                    [action.payload.screenId]: newScreen,
+                };
+            }
+    
+            case actionTypes.RESIZE_SCREEN: {
+                const newScreen = getScreenCloneByPayloadScreenId();
+                newScreen.width = action.payload.newWidth;
+                newScreen.height = action.payload.newHeight;
+    
+                if (action.payload.newX !== null) {
+                    newScreen.x = action.payload.newX;
+                }
+    
+                if (action.payload.newY !== null) {
+                    newScreen.y = action.payload.newY;
+                }
+    
+    
+                return {
+                    ...state,
+                    [action.payload.screenId]: newScreen,
+                };
+            }
+    
+            case actionTypes.LOAD_SCREENS: {
+                return Object.keys(action.payload.screensData.list)
+                        .map(screenId => ScreenModel.createFromJSON({
+                            ...action.payload.screensData.list[screenId],
+                            id: screenId,
+                        }))
+                        .reduce((newState, screen) => ({
+                            ...newState,
+                            [screen.id]: screen,
+                        }), {});
+            }
+    
+            case linkActionTypes.ADD_LINK: {
+                const newScreen = getScreenCloneByPayloadScreenId();
+                newScreen.linkIds.push(action.payload.link.id);
+    
+                return {
+                    ...state,
+                    [action.payload.screenId]: newScreen,
+                };
+            }
+    
+            case linkActionTypes.DELETE_LINK: {
+                return Object.keys(state).reduce((allScreens, screenId) => {
+                    const clone = getScreenCloneByPayloadScreenId(screenId);
+                    clone.linkIds = clone.linkIds.filter(linkId => linkId !== action.payload.linkId);
+    
                     return {
-                        ...newState,
-                        [clonedScreen.id]: clonedScreen,
+                        ...allScreens,
+                        [screenId]: clone,
                     };
                 }, {});
-        }
-
-        case actionTypes.MOVE_SCREEN: {
-            const newScreen = state[action.payload.screenId].clone();
-
-            newScreen.x = Math.max(0, action.payload.newX);
-            newScreen.y = Math.max(0, action.payload.newY);
-
-            return {
-                ...state,
-                [action.payload.screenId]: newScreen,
-            };
-        }
-
-        case actionTypes.SELECT_SCREEN: {
-            const newScreen = state[action.payload.screenId].clone();
-
-            newScreen.isSelected = true;
-
-            return {
-                ...state,
-                [action.payload.screenId]: newScreen,
-            };
-        }
-
-        case actionTypes.UNSELECT_SCREEN: {
-            const newScreen = state[action.payload.screenId].clone();
-
-            newScreen.isSelected = false;
-
-            return {
-                ...state,
-                [action.payload.screenId]: newScreen,
-            };
-        }
-
-        case actionTypes.RESIZE_SCREEN: {
-            const newScreen = state[action.payload.screenId].clone();
-            newScreen.width = action.payload.newWidth;
-            newScreen.height = action.payload.newHeight;
-
-            if (action.payload.newX !== null) {
-                newScreen.x = action.payload.newX;
             }
-
-            if (action.payload.newY !== null) {
-                newScreen.y = action.payload.newY;
+    
+            case logicActionTypes.ADD_RULE: {
+                const newScreen = getScreenCloneByPayloadScreenId();
+                newScreen.logicRuleIds.push(action.payload.rule.id);
+    
+                return {
+                    ...state,
+                    [action.payload.screenId]: newScreen,
+                };
             }
-
-
-            return {
-                ...state,
-                [action.payload.screenId]: newScreen,
-            };
+    
+            case logicActionTypes.DELETE_RULES: {
+                return Object.keys(state).reduce((allScreens, screenId) => {
+                    const clone = getScreenCloneByPayloadScreenId(screenId);
+    
+                    clone.logicRuleIds = clone.logicRuleIds.filter(ruleId => (
+                        action.payload.ruleIds.indexOf(ruleId) === -1
+                    ));
+    
+                    return {
+                        ...allScreens,
+                        [screenId]: clone,
+                    };
+                }, {});
+            }
+    
+            default:
+                return state;
+        }
+    } catch (e) {
+        if (!(e instanceof ReferenceError)) {
+            console.error(e);
         }
 
-        case actionTypes.LOAD_SCREENS: {
-            return Object.keys(action.payload.screensData.list)
-                    .map(screenId => ScreenModel.createFromJSON({
-                        ...action.payload.screensData.list[screenId],
-                        id: screenId,
-                    }))
-                    .reduce((newState, screen) => ({
-                        ...newState,
-                        [screen.id]: screen,
-                    }), {});
-        }
-
-        case linkActionTypes.ADD_LINK: {
-            const newScreen = state[action.payload.screenId].clone();
-            newScreen.linkIds.push(action.payload.link.id);
-
-            return {
-                ...state,
-                [action.payload.screenId]: newScreen,
-            };
-        }
-
-        case linkActionTypes.DELETE_LINK: {
-            return Object.keys(state).reduce((allScreens, screenId) => {
-                const clone = state[screenId].clone();
-                clone.linkIds = clone.linkIds.filter(linkId => linkId !== action.payload.linkId);
-
-                return {
-                    ...allScreens,
-                    [screenId]: clone,
-                };
-            }, {});
-        }
-
-        case logicActionTypes.ADD_RULE: {
-            const newScreen = state[action.payload.screenId].clone();
-            newScreen.logicRuleIds.push(action.payload.rule.id);
-
-            return {
-                ...state,
-                [action.payload.screenId]: newScreen,
-            };
-        }
-
-        case logicActionTypes.DELETE_RULES: {
-            return Object.keys(state).reduce((allScreens, screenId) => {
-                const clone = state[screenId].clone();
-
-                clone.logicRuleIds = clone.logicRuleIds.filter(ruleId => (
-                    action.payload.ruleIds.indexOf(ruleId) === -1
-                ));
-
-                return {
-                    ...allScreens,
-                    [screenId]: clone,
-                };
-            }, {});
-        }
-
-        default:
-            return state;
+        return state;
     }
 }
